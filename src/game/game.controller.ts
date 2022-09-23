075d3263b7cc499428dbffdc883990b139731be3
Taneli <13731577+tlzdev@users.noreply.github>
2022-09-23 23:23:11 +0300

    feat: include release data from api, add new endpoint GET /game/:id

--- src/game/game.controller.ts
+++ src/game/game.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Post, Param, NotFoundException } from '@nestjs/common';
+import { Controller, Get, Param, NotFoundException } from '@nestjs/common';
 import { Game } from '@prisma/client';
 import { GameService } from './game.service';
+import { ParseUUIDPipe } from '@nestjs/common/pipes';
 
-import { IsString, IsDefined, IsNotEmpty } from 'class-validator';
+import { IsString, IsDefined, IsNotEmpty, UUIDVersion } from 'class-validator';
 
 export class GameDto {
   @IsString()
@@ -15,20 +16,32 @@ export class GameDto {
 export class GameController {
   constructor(private readonly gameService: GameService) {}
 
-  @Post('game/by-exact-name/:name')
-  public async fetchGameByExactName(@Param('name') name: string): Promise<Game | null> {
+  @Get('/game/:id')
+  public async fetchGameById(@Param('id', ParseUUIDPipe) id: string): Promise<Game> {
+    const game: Game = await this.gameService.fetchGameById(id);
+
+    if (!game) {
+      // As always, it's up for debate whether "No resource found" should return 404 or 200 with empty response,
+      // in this case decided to go with 404
+      throw new NotFoundException('No game for given id');
+    }
+    return game;
+  }
+
+  @Get('/game/by-exact-name/:name')
+  public async fetchGameByExactName(@Param('name') name: string): Promise<Game> {
     const game: Game = await this.gameService.fetchGameByExactName(name);
 
     if (!game) {
-      // As always, it's up for debate whether "No resource found" should return 404 or 200 with empty response, 
+      // As always, it's up for debate whether "No resource found" should return 404 or 200 with empty response,
       // in this case decided to go with 404
       throw new NotFoundException('No game for given name');
     }
     return game;
   }
 
-  @Post('game/search/:name')
-  public searchGamesByFuzzyName(@Param('name') name: string): Promise<Game[] | null> {
+  @Get('/game/search/:name')
+  public searchGamesByFuzzyName(@Param('name') name: string): Promise<Game[]> {
     return this.gameService.searchGamesByFuzzyName(name);
   }
 }
